044fee59af2a11191f07e436f8ca78f62d69aab7
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-11-04 19:10:29 +0800

    initial commit

---
+++ infoCollect.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Math";
+let classSchedule = "10:00 AM - 12:00 PM, Monday and Wednesday";
+let classroom = "ComlabA";
+let classInstructor = "Ramil Batin";
+let studentName = "Aaron Raymund R. Paguirigan";
+
+console.log(studentName + " is currently enrolled in " + subjectTitle + " with a class schedule of " + classSchedule + " at room " + classroom + ". The instructor for the subject is " + classInstructor)
